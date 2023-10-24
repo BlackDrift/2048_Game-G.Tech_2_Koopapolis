d58d580d9f13a912eb42f98b9770307220493c64
Julien <Jfabregoule@gaming.tech>
2023-10-24 13:34:14 +0200

    Tiles files added

---
+++ Tile.h
@@ -0,0 +1,21 @@
+#ifndef TILE_H
+# define TILE_H
+
+#include "main.h"
+
+class Tile
+{
+public:
+	//Attributes
+	int	value;
+
+	//Constructor
+	Tile(int);
+
+	//Method
+
+
+	//Destructor
+};
+
+#endif
